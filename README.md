46b1492b8eb4e1dd9733def6a2480dcfd95e6f66
PamaLee <pama@pamalee.cn>
2022-10-26 19:42:34 +0800

    Create the Project!

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # WhyNotTeaming
+
 A project to form a simple group for work
 
 This is a test project
---
+++ src/main/java/cn/devspace/whynotteaming/WhyNotTeamingApplication.java
@@ -0,0 +1,13 @@
+package cn.devspace.whynotteaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyNotTeamingApplication {
+//入口类
+    public static void main(String[] args) {
+        SpringApplication.run(WhyNotTeamingApplication.class, args);
+    }
+
+}
